81ec716e3d733d3952ed91b0c2106570f3194f45
SunflowerKun <dream26322gmail.com>
2022-05-10 21:08:17 +0600

    add objects

--- src/main/java/peaksoft/App.java
+++ src/main/java/peaksoft/App.java
@@ -2,13 +2,28 @@ package peaksoft;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+
+ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        Cat cat =  context.getBean("cat", Cat.class);
+        System.out.println(cat.getName());
+        System.out.println(cat.getAge());
+
+        Cat cat1 = context.getBean("cat", Cat.class);
+        System.out.println(cat1.getName());
+        System.out.println(cat1.getAge());
+
+        Boolean comparison = cat.equals(cat1);
+        System.out.println("comparison = " + comparison);
+
+
     }
 }
